ef4936182550fb147ede0a55a6a37bb70b677603
channa <channa@qarik.com>
2020-09-21 23:33:56 +0100

    Typo correction in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 //   description   = "The public IP address of the web server"
 // }
 
-output "aln_dns_name" {
+output "alb_dns_name" {
   value       = aws_lb.example.dns_name
   description = "The domain name of the load balancer"
 }
