9e15635395a275993d013088f44f88d2bfbb3942
channa <channa@qarik.com>
2020-09-22 11:34:12 +0100

    C3 S3 & DynamoDB

--- main.tf
+++ main.tf
@@ -1,183 +1,182 @@
-terraform {
-    required_version = ">= 0.12, < 0.13"
-}
+// terraform {
+//     required_version = ">= 0.12, < 0.13"
+// }
     
 
-provider "aws" {
-    profile = "default"
-    region  = "us-east-2"
+// provider "aws" {
+//     profile = "default"
+//     region  = "us-east-2"
 
-    # Allow any 2.x version of the AWS provider
-    version = "~> 2.0"
-}
+//     # Allow any 2.x version of the AWS provider
+//     version = "~> 2.0"
+// }
 
-// resource "aws_instance" "example" {
-//     ami             = "ami-0c55b159cbfafe1f0"
+// // resource "aws_instance" "example" {
+// //     ami             = "ami-0c55b159cbfafe1f0"
+// //     instance_type   =   "t2.micro"
+// //     vpc_security_group_ids = [aws_security_group.instance.id]
+// //     user_data = <<-EOF
+// //                 #!/bin/bash
+// //                 echo "Hello, World of Inifinite Possibilites" > index.html
+// //                 nohup busybox httpd -f -p ${var.server_port} &
+// //                 EOF
+       
+// //     tags    = {
+// //         Name        = "terraform-example-instance",
+// //         QarikUser   = "channa"
+// //     }
+// // }
+
+// resource "aws_security_group" "instance" {
+//     name = var.instance_security_group_name
+
+//     ingress {
+//         from_port = var.server_port
+//         to_port   = var.server_port
+//         protocol  = "tcp"
+//         cidr_blocks = ["0.0.0.0/0"]
+//     }
+
+//     tags    = {
+//         Name      = "terraform-example-instance-sg",
+//         QarikUser = "channa"
+//     }
+// }
+
+// resource "aws_launch_configuration" "example" {
+//     image_id        = "ami-0c55b159cbfafe1f0"
 //     instance_type   =   "t2.micro"
-//     vpc_security_group_ids = [aws_security_group.instance.id]
+//     security_groups = [aws_security_group.instance.id]
 
 //     user_data = <<-EOF
 //                 #!/bin/bash
 //                 echo "Hello, World of Inifinite Possibilites" > index.html
 //                 nohup busybox httpd -f -p ${var.server_port} &
 //                 EOF
+//     # Required when using a launch configuration with an autoscaling group.
+//     # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
        
-//     tags    = {
-//         Name        = "terraform-example-instance",
-//         QarikUser   = "channa"
-//     }
+//     lifecycle {
+//       create_before_destroy = true
+//       }   
 // }
 
-resource "aws_security_group" "instance" {
-    name = var.instance_security_group_name
+// data "aws_vpc" "default" {
+//   default = true
+// }
 
-    ingress {
-        from_port = var.server_port
-        to_port   = var.server_port
-        protocol  = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+// data "aws_subnet_ids" "default" {
+//   vpc_id = data.aws_vpc.default.id
+// }
 
-    tags    = {
-        Name      = "terraform-example-instance-sg",
-        QarikUser = "channa"
-    }
-}
-
-resource "aws_launch_configuration" "example" {
-    image_id        = "ami-0c55b159cbfafe1f0"
-    instance_type   =   "t2.micro"
-    security_groups = [aws_security_group.instance.id]
-
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World of Inifinite Possibilites" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
-    # Required when using a launch configuration with an autoscaling group.
-    # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
-       
-    lifecycle {
-      create_before_destroy = true
-      }   
-}
-
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+// resource "aws_autoscaling_group" "example" {
+//   launch_configuration = aws_launch_configuration.example.name
+//   vpc_zone_identifier  = data.aws_subnet_ids.default.ids
   
-  target_group_arns = [aws_lb_target_group.asg.arn]
-  health_check_type = "ELB"
+//   target_group_arns = [aws_lb_target_group.asg.arn]
+//   health_check_type = "ELB"
   
-  min_size = 2
-  max_size = 10
+//   min_size = 2
+//   max_size = 10
   
-  tag {
-    key                  = "Name"
-    value                = "terraform-example-asg"
-    propagate_at_launch  = true
-    }  
-}
+//   tag {
+//     key                  = "Name"
+//     value                = "terraform-example-asg"
+//     propagate_at_launch  = true
+//     }  
+// }
 
-resource "aws_lb" "example" {
-  name = "terraform-example-asg"
-  load_balancer_type = "application"
-  subnets = data.aws_subnet_ids.default.ids
-  security_groups     = [aws_security_group.alb.id]
+// resource "aws_lb" "example" {
+//   name = "terraform-example-asg"
+//   load_balancer_type = "application"
+//   subnets = data.aws_subnet_ids.default.ids
+//   security_groups     = [aws_security_group.alb.id]
 
-  tags = {
-      Name      = "terraform-example-lb",
-      QarikUser = "channa"
-  }
-}
+//   tags = {
+//       Name      = "terraform-example-lb",
+//       QarikUser = "channa"
+//   }
+// }
 
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = "aws_lb.example.arn"
-  port              = 80
-  protocol          = "HTTP"
+// resource "aws_lb_listener" "http" {
+//   load_balancer_arn = "aws_lb.example.arn"
+//   port              = 80
+//   protocol          = "HTTP"
   
-  # By default, return a simple 404 page
-  default_action {
-    type = "fixed-response"
+//   # By default, return a simple 404 page
+//   default_action {
+//     type = "fixed-response"
     
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
+//     fixed_response {
+//       content_type = "text/plain"
+//       message_body = "404: page not found"
+//       status_code  = 404
+//     }
+//   }
+// }
 
-resource "aws_security_group" "alb" {
-  name = "terraform-example-alb"
+// resource "aws_security_group" "alb" {
+//   name = "terraform-example-alb"
   
-  # Allow inbound HTTP requests
-  ingress {
-    from_port     = 80
-    to_port       = 80
-    protocol      = "tcp"
-    cidr_blocks   = ["0.0.0.0/0"]
-  } 
+//   # Allow inbound HTTP requests
+//   ingress {
+//     from_port     = 80
+//     to_port       = 80
+//     protocol      = "tcp"
+//     cidr_blocks   = ["0.0.0.0/0"]
+//   } 
   
-  # Allow all outbound requests
-  egress {
-    from_port     = 0
-    to_port       = 0
-    protocol      = "-1"
-    cidr_blocks   = ["0.0.0.0/0"]
-  }
+//   # Allow all outbound requests
+//   egress {
+//     from_port     = 0
+//     to_port       = 0
+//     protocol      = "-1"
+//     cidr_blocks   = ["0.0.0.0/0"]
+//   }
 
-     tags    = {
-        Name      = "terraform-example-lb-sg",
-        QarikUser = "channa"
-    }
+//      tags    = {
+//         Name      = "terraform-example-lb-sg",
+//         QarikUser = "channa"
+//     }
 
-}
+// }
 
-resource "aws_lb_target_group" "asg" {
-  name      = "terraform-asg-example"
-  port      = var.server_port
-  protocol  = "HTTP"
-  vpc_id    = data.aws_vpc.default.id
+// resource "aws_lb_target_group" "asg" {
+//   name      = "terraform-asg-example"
+//   port      = var.server_port
+//   protocol  = "HTTP"
+//   vpc_id    = data.aws_vpc.default.id
   
-  health_check  {
-    path                = "/"
-    protocol            = "HTTP"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 3
-    healthy_threshold   = 2    
-    unhealthy_threshold = 2
-  }
+//   health_check  {
+//     path                = "/"
+//     protocol            = "HTTP"
+//     matcher             = "200"
+//     interval            = 15
+//     timeout             = 3
+//     healthy_threshold   = 2    
+//     unhealthy_threshold = 2
+//   }
 
-  tags = {
-      Name = "terraform-example-lb-tg"
-      QarikUser = "channa"
-  }
-}
+//   tags = {
+//       Name = "terraform-example-lb-tg"
+//       QarikUser = "channa"
+//   }
+// }
 
-resource "aws_lb_listener_rule" "asg" {
-  listener_arn = aws_lb_listener.http.arn
-  priority     = 100
+// resource "aws_lb_listener_rule" "asg" {
+//   listener_arn = aws_lb_listener.http.arn
+//   priority     = 100
 
-  condition {
-    path_pattern {
-        values = ["*"]
-    }
-  } 
+//   condition {
+//     path_pattern {
+//         values = ["*"]
+//     }
+//   } 
   
-  action {
-    type              = "forward"
-    target_group_arn  = aws_lb_target_group.asg.arn
-  }
-}
+//   action {
+//     type              = "forward"
+//     target_group_arn  = aws_lb_target_group.asg.arn
+//   }
+// }
 
 
